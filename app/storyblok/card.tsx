5d8e117b1617a4ee6c9a4ed56cc55e835880a4d7
Timo Westland <timo.westland@saltagency.io>
2024-01-25 16:04:32 +0100

    fix: type issues

--- app/storyblok/card.tsx
+++ app/storyblok/card.tsx
@@ -1,11 +1,16 @@
 import { Card } from '~/components/card'
 import type { CardBlok } from '~/types'
+import { mapLink, useLocalizedMappers } from '~/utils/mappers'
 import { StoryBlokWrapper } from '~/utils/storyblok'
 
 export function SbCard({ blok }: { blok: CardBlok }) {
+  const { mapLink } = useLocalizedMappers()
+  const { link, ...rest } = blok
   return (
     <StoryBlokWrapper blok={blok}>
-      <Card {...blok}>{blok.body}</Card>
+      <Card {...rest} link={link && link[0] ? mapLink(link[0]) : undefined}>
+        {blok.body}
+      </Card>
     </StoryBlokWrapper>
   )
 }
