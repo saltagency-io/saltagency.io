1035c358892ede075fac2518d587b13d845943d7
Nino <hello@n1n0.codes>
2024-07-25 19:41:41 +0200

    Fix

--- app/components/rich-text.tsx
+++ app/components/rich-text.tsx
@@ -25,7 +25,7 @@ export function RichText({ content, title, subtitle, text }: Props) {
   const viewPort = { once: true, margin: '-115px 0px' }
   return (
     <Grid className="max-w-6xl">
-      <div className="col-span-full px-16 lg:col-span-10 lg:col-start-2">
+      <div className="col-span-full px-4 md:px-16 lg:col-span-10 lg:col-start-2">
         <motion.div
           className="mb-8 space-y-4"
           initial="initial"
